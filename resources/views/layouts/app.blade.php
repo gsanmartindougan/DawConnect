79585b0ccf22c51bca8891744c19f387b3f394c2
gsanmartindougan <144922246+gsanmartindougan@users.noreply.github.com>
2024-05-09 16:17:17 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,9 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <link rel="stylesheet" href="asset('css/summernote-lite.css', true)">
-    <link rel="stylesheet" href="asset('css/bootstrap.css', true)">
-    <link rel="stylesheet" href="asset('css/styles.css', true)">
+    <link rel="stylesheet" href="https://dawconnect.up.railway.app/css/summernote-lite.css">
+    <link rel="stylesheet" href="https://dawconnect.up.railway.app/css/bootstrap.css">
+    <link rel="stylesheet" href="https://dawconnect.up.railway.app/css/styles.css">
 </head>
 
 <body>
@@ -29,8 +29,8 @@
             @endif
         </main>
     </div>
-    <script src="asset('js/summernote-lite.js', true)"></script>
-    <script src="asset('js/bootstrap.bundle.js', true)"></script>
+    <script src="https://dawconnect.up.railway.app/js/summernote-lite.js"></script>
+    <script src="https://dawconnect.up.railway.app/js/bootstrap.bundle.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
      {{-- @include('layouts.footer') --}}
     <script>
