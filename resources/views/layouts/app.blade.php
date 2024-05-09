191e9a8f9ea784575b18777e86c22cd4722e2c83
gsanmartindougan <144922246+gsanmartindougan@users.noreply.github.com>
2024-05-09 02:02:44 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,9 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <link rel="stylesheet" href="{{css/summernote-lite.css'}}">
-    <link rel="stylesheet" href="{{'css/bootstrap.css'}}">
-    <link rel="stylesheet" href="{{'css/styles.css'}}">
+    <link rel="stylesheet" href="css/summernote-lite.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
@@ -29,8 +29,8 @@
             @endif
         </main>
     </div>
-    <script src="{{'js/summernote-lite.js'}}"></script>
-    <script src="{{'js/bootstrap.bundle.js'}}"></script>
+    <script src="js/summernote-lite.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
      {{-- @include('layouts.footer') --}}
     <script>
