6a78ec6d3f8e98235e9775523e1d9c5f02bee3a1
gsanmartindougan <sanmarting2@gmail.com>
2024-05-10 21:19:09 +0200

    app.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,10 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <link rel="stylesheet" href="asset('css/summernote-lite.css', true)">
-    <link rel="stylesheet" href="asset('css/bootstrap.css', true)">
-    <link rel="stylesheet" href="asset('css/styles.css', true)">
+    <link rel="stylesheet" href="{{asset('css/summernote-lite.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link href="{{asset('css/datatables.min.css')}}" rel="stylesheet">
 </head>
 
 <body>
@@ -25,12 +26,14 @@
         <main class="py-2">
             @yield('content')
             @if (auth()->user())
-                @include('layouts.action')
+                {{-- @include('layouts.action') --}}
+                {{-- @include('pages.post.create-modal') --}}
             @endif
         </main>
     </div>
-    <script src="asset('js/summernote-lite.js', true)"></script>
-    <script src="asset('js/bootstrap.bundle.js', true)"></script>
+    <script src="{{asset('js/summernote-lite.js')}}"></script>
+    <script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+    <script src="{{asset('js/datatables.min.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
      {{-- @include('layouts.footer') --}}
     <script>
